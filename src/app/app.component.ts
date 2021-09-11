b7093924ad38a3e924b2ee5ae1459ec100a06eac
Andres Benintende <andresbenintende@gmail.com>
2021-09-11 11:13:29 -0300

    + Cambios en release2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'isw-g1';
+  title = 'Modificado desde release2 isw-g1';
 }
